fa7592c3bf25f05d2993370d93bd50877a75f90c
SukmaAyundaSafitri <sukmaayunda28gmail.com>
2019-05-23 10:25:29 +0700

    Create index.php
    pembuatan index

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	session_start();
+	date_default_timezone_set("Asia/Jakarta");
+	
+	include 'cores/Response.php';
+	include 'cores/Router.php';
+	include 'cores/Input.php';
+	include 'cores/ORM.php';
+	include 'cores/App.php';
+	include 'define.php';
+	include 'routers.php'; 
